68fcef484f586054e1bc7d8d481f5ac7d9f7d457
WireBits <159493381+wirebits@users.noreply.github.com>
2026-01-17 20:01:58 +0530

    Enhance boot.py to disable serial console
    Updated boot.py to disable serial console along with USB drive.

--- boot.py
+++ boot.py
@@ -1,11 +1,12 @@
 # BLEDuck boot.py file
-# Setup for hide / unhide mass storage device.
+# Setup for hide / unhide mass storage device and disable serial console.
 # Author - WireBits
 
-import board, storage, digitalio
+import board, storage, digitalio, usb_cdc
 
-button=digitalio.DigitalInOut(board.GPX)
+button=digitalio.DigitalInOut(board.IOX)
 button.switch_to_input(pull=digitalio.Pull.UP)
 
 if button.value:
+    usb_cdc.disable()
     storage.disable_usb_drive()
